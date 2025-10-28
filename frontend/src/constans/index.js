d246e87fadf55efec4291950d97914d6fee25208
javed-shekh777 <mdjavedshekh777@gmail.com>
2025-10-28 11:02:37 +0530

    Changed back/fronte host url

--- frontend/src/constans/index.js
+++ frontend/src/constans/index.js
@@ -1,4 +1,4 @@
 // export const BACKEND_URL = "http://localhost:8080/api"; 
-export const BACKEND_URL = "https://cookifybac.vercel.app"; 
+export const BACKEND_URL = "https://cookifybac.vercel.app/api"; 
 
 
