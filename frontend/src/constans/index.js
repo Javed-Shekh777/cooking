a99b92a56a8a3748edd38671fc957ab994eb7c5d
javed-shekh777 <mdjavedshekh777@gmail.com>
2025-11-16 20:07:20 +0530

    big

--- frontend/src/constans/index.js
+++ frontend/src/constans/index.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "http://localhost:8080/api"; 
-// export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL ||  "https://cookifybac.vercel.app/api"; 
+// export const BACKEND_URL = "http://localhost:8080/api"; 
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL ||  "localhost:8080/api"; 
 
 
